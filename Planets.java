0daecb031819a3c8a5f34f46024409e97597ca93
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-11-30 11:23:26 -0500

    print array no work

--- Planets.java
+++ Planets.java
@@ -1,10 +1,6 @@
 class Planets {
     public static void main(String[] args) {
         String[] planets = { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto" };
-        System.out.println("Original Array:");
-        for (String planet: planets) {
-            System.out.print(planet + " ");
-        }
         insertSort(planets);
     }
 
@@ -12,15 +8,24 @@ class Planets {
         for (int top = 1; top < planets.length; top++) {
             String item = planets[top];
             int i = top - 1;
+
             while (i >= 0 && planets[i].compareTo(item) > 0) {
                 planets[i + 1] = planets[i];
                 i = i - 1;
             }
+
             planets[i + 1] = item;
+            printArray(planets);
         }
-        System.out.println("\n\nSorted Array");
-        for (String planet : planets) {
-            System.out.print(planet + " ");
+    }
+
+    private static void printArray(String[] array) {
+        for (int k = 0; k < array.length; k++) {
+            System.out.println(array[k]);
+            if (k < array.length - 1) {
+                System.out.println(" ");
+            }
         }
+        System.out.println();
     }
 }
